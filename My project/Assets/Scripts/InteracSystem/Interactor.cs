9537dc9a916bc9ed3e17f702937cf363112cc9d6
RenattoPadilha <rspf@cin.ufpe.br>
2024-03-06 13:41:20 -0300

    Incomplete Update in Mov System

--- My project/Assets/Scripts/InteracSystem/Interactor.cs
+++ My project/Assets/Scripts/InteracSystem/Interactor.cs
@@ -6,8 +6,47 @@ using UnityEngine;
 public class Interactor : MonoBehaviour
 {
     private int _layerNumber = 8;
+    private InteractionPromptUI interactionPromptUI;
+    [SerializeField] private Transform _interactionPoint;
+    [SerializeField] private float _interactionPointRadius = 0.5f; 
+    private Collider[] _colliders = new Collider [5];
+    private int _numFound;
+
+    void Start(){
+      interactionPromptUI = GameObject.Find("Canvas").GetComponent<InteractionPromptUI>();
+    }
+
+    private void Update()
+    {
+        _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, 1<<_layerNumber);
+
+        if (_numFound > 0)
+        {
+          var clickedObject = _colliders[0].GetComponent<IInteractable>();
+          interactionPromptUI.SetUp(clickedObject.InteractionPrompt);
+          
+          if (clickedObject != null && Input.GetKeyDown(KeyCode.E))
+            clickedObject.Interact(this);          
+        }
+        else
+          interactionPromptUI.Close();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_interactionPoint.position, _interactionPointRadius);
+    }
+
+/*
+    *For player without legs movement (only camera)
+    private int _layerNumber = 8;
     private RaycastHit hitObject;
-    [SerializeField] private InteractionPromptUI interactionPromptUI;
+    private InteractionPromptUI interactionPromptUI;
+
+    void Start(){
+      interactionPromptUI = GameObject.Find("Canvas").GetComponent<InteractionPromptUI>();
+    }
 
      private void Update()
     {
@@ -24,39 +63,7 @@ public class Interactor : MonoBehaviour
           interactionPromptUI.Close();
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        //Gizmos.DrawRay();
-    }
+*/
 
-
-    /*
-    * Codigo para interacao com um player que se movimenta
-    [SerializeField] private Transform _interactionPoint;
-    [SerializeField] private float _interactionPointRadius = 0.5f;
-    private Collider[] _colliders = new Collider [5];
-    private int _numFound;
-
-    private void Update()
-    {
-        _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, _interactableMask);
-
-        if (_numFound > 0)
-        {
-          var interactable = _colliders[0].GetComponent<IInteractable>();
-          
-          if (interactable != null && Keyboard.current.eKey.wasPressedThisFrame)
-          {
-            interactable.Interact(this);
-          
-        }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(_interactionPoint.position, _interactionPointRadius);
-    }
-    */
+    
 }
--- My project/Assets/Scripts/MovSystem/PlayerMovement.cs
+++ My project/Assets/Scripts/MovSystem/PlayerMovement.cs
@@ -1,43 +1,54 @@
 
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f; // Velocidade de movimento do jogador
+    private int wallsLayer;
+
+    void Start (){
+      int scenarioLayer = 1 << 7;
+      int interactableScenarioLayer = 1 << 8;
+      wallsLayer = scenarioLayer | interactableScenarioLayer;
+    }
 
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
+        if(IsCollidingWithScenario(new Vector3(horizontalInput,0f,0f).normalized)){
+          horizontalInput = 0f;
+        }
+        if(IsCollidingWithScenario(new Vector3(0f,0f,verticalInput).normalized)){
+          verticalInput = 0f;
+        }
 
         Vector3 movementDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
 
-        Vector3 movement = movementDirection * moveSpeed * Time.deltaTime;
-        transform.Translate(movement);
-
-        if (IsCollidingWithWall())
-        {
-            Debug.Log("Sim, confirmo");
-            Vector3 movementDirectS = new Vector3(horizontalInput, 0f, -1f).normalized;
-            Vector3 movementS = movementDirectS * moveSpeed * Time.deltaTime;
-
-            transform.Translate(movementS);
+        if (!IsCollidingWithScenario(movementDirection)){
+          Vector3 movement = movementDirection * moveSpeed * Time.deltaTime;
+          transform.Translate(movement);
         }
     }
 
-    private bool IsCollidingWithWall()
+    private bool IsCollidingWithScenario(Vector3 movementDirection)
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 0.5f))
+        Debug.DrawLine(transform.position, movementDirection*0.5f,Color.magenta);
+        Debug.Log(transform.forward);
+        if (Physics.Raycast(transform.position, movementDirection, out hit, 0.5f, wallsLayer))
         {
-            if (hit.collider.CompareTag("Wall"))
-            {
-                Debug.Log("AQUIII");
-                return true;
-            }
+            Debug.Log("AQUIII");
+            return true;
         }
 
         return false;
+    }
+
+    public void OnDrawGizmos(){
+      Gizmos.color = Color.red;
+      //Gizmos.DrawRay(transform.position, transform.forward *0.5f);
     }
 }
