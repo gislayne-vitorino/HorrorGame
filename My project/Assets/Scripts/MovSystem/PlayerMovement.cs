0908cbac4680926bb35fbd511b7feaf01b732eec
yasmimvso <yvso@cin.ufpe.br>
2024-03-16 17:10:05 -0300

    feat: sounds, main screen start and footstep working

--- My project/Assets/Scripts/MovSystem/PlayerMovement.cs
+++ My project/Assets/Scripts/MovSystem/PlayerMovement.cs
@@ -49,7 +49,6 @@ public class PlayerMovement : MonoBehaviour
         {
             if (hit.collider.CompareTag("Wall"))
             {
-                Debug.Log("AQUIII");
                 return true;
             }
         }
@@ -61,62 +60,3 @@ public class PlayerMovement : MonoBehaviour
       //Gizmos.DrawRay(transform.position, transform.forward *0.5f);
     }
 }
-
-//-------------------------------------------------
-/*using UnityEngine;
-
-public class PlayerMovement : MonoBehaviour
-{
-    public float moveSpeed = 5f; // Velocidade de movimento do jogador
-
-    void Update()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        Vector3 movementDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
-        Vector3 movement = movementDirection * moveSpeed * Time.deltaTime;
-
-        // Verifica se houve colisão em alguma direção e impede o movimento nessa direção
-        if (IsCollidingWithWall(transform.forward) && verticalInput > 0f)
-        {
-            movement.z = 0f;
-        }
-        if (IsCollidingWithWall(-transform.forward) && verticalInput < 0f)
-        {
-            movement.z = 0f;
-        }
-        if (IsCollidingWithWall(transform.right) && horizontalInput > 0f)
-        {
-            movement.x = 0f;
-        }
-        if (IsCollidingWithWall(-transform.right) && horizontalInput < 0f)
-        {
-            movement.x = 0f;
-        }
-
-        transform.Translate(movement);
-    }
-
-    private bool IsCollidingWithWall(Vector3 direction)
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, direction, out hit, 0.5f))
-        {
-            if (hit.collider.CompareTag("Wall"))
-            {
-                Debug.Log("AQUIII");
-                return true;
-            }
-        }
-        return false;
-    }
-
-     Debug.DrawLine(transform.position, movementDirection*0.5f,Color.magenta);
-            Debug.Log(transform.forward);
-            if (Physics.Raycast(transform.position, movementDirection, out hit, 0.5f, wallsLayer))
-            {
-                Debug.Log("AQUIII");
-                return true;
-            }
-}*/
